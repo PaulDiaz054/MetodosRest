5240900fa3bbd1be0b03eaa544b523d20bb2182d
PaulDiaz054 <pd1004405054@gmail.com>
2025-01-08 10:44:08 -0500

    subida de los ejercicios

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3000, () => {
+var users = [
+  { id: 1, name: 'Paul' },
+  { id: 2, name: 'David' },
+  { id: 3, name: 'Mateo' }
+];
+
+app.delete('/delete/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const userId = users.findIndex(user => user.id === id);
+
+  if (userId !== -1) {
+    users.splice(userId, 1);
+    res.send("Usuario Eliminado");
+  } else {
+    res.send("Usuario no encontrado");
+  }
+});
+
+app.listen(4000, () => {
   console.log("Servicio corriendo...");
 });
